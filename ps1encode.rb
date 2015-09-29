88088f6a579457c3049d3ee5f46a17fc9fc01167
Tyler Schmidtke <tylerschmidtke@gmail.com>
2015-09-28 21:23:11 -0500

    update ps1encode;rb
    Adding support for new MinGW path in Kali 2.0.

--- ps1encode.rb
+++ ps1encode.rb
@@ -246,8 +246,8 @@ end
 ########################EXE_ENCODE###############################
 if $lencode == "exe"
 
-#determine if MinGW has been installed
-mingw = Dir::exists?('/usr/i586-mingw32msvc')
+#determine if MinGW has been installed, support new and old MinGW system paths
+mingw = True if Dir::exists?('/usr/i586-mingw32msvc') || Dir::exists?('/usr/bin/i586-migw32msvc')
 if mingw == false
     puts "Must have MinGW installed in order to compile EXEs!!"
     puts "\n\tRun to download: apt-get install mingw32 \n"
