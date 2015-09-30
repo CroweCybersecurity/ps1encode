0e238a96171343bdaaf4269c559bbfd8ef1c1ce5
Peter Marszalik <addenial@gmail.com>
2015-09-30 09:19:36 -0500

    Update ps1encode.rb

--- ps1encode.rb
+++ ps1encode.rb
@@ -247,7 +247,7 @@ end
 if $lencode == "exe"
 
 #determine if MinGW has been installed, support new and old MinGW system paths
-mingw = File::exists?('/usr/i586-mingw32msvc') || File::exists?('/usr/bin/i586-migw32msvc')
+mingw = true if File::exists?('/usr/i586-mingw32msvc') || File::exists?('/usr/bin/i586-migw32msvc')
 if mingw == false
     puts "Must have MinGW installed in order to compile EXEs!!"
     puts "\n\tRun to download: apt-get install mingw32 \n"
